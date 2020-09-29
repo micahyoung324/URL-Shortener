9061baf779620e02b68119dd59407861a86f0407
Micah Wood <micahwood50@gmail.com>
2020-09-29 19:15:07 -0400

    Implemented POST method to homepage

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template("home.html")
+    if request.method == "POST":
+        url_received = request.form["nm"]
+        return url_received
+    else:
+        return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(port = 5000, debug = True)
